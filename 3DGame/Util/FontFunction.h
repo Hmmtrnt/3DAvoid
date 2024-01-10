503756c3b73ecd4cb7d5d175240220b8745de9ee
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-01-10 12:18:42 +0900

    コメント追加

--- 3DGame/Util/FontFunction.h
+++ 3DGame/Util/FontFunction.h
@@ -1,5 +1,5 @@
-#pragma once
 // フォントデータ読み込み(フォントは創英角ポップ体に固定)
+#pragma once
 
 class StringInit
 {
@@ -10,10 +10,15 @@ public:
 	/// <summary>
 	/// フォントデータ作成
 	/// </summary>
+	/// <param name="fontHandle">画像のパス</param>
 	/// <param name="fontSize">サイズ</param>
 	/// <param name="fontThick">太さ</param>
 	/// <param name="fontType">タイプ</param>
 	void Init(int& fontHandle, int fontSize, int fontThick, int fontType);
-	// フォントデータ削除
+
+	/// <summary>
+	/// フォントデータ削除
+	/// </summary>
+	/// <param name="fontHandle">画像のパス</param>
 	void End(int& fontHandle);
 };
