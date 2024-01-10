051f22bb5a6cd17998f8dc9eb34f7045d24982b2
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-01-10 12:19:33 +0900

    ゲームファイルのリファクタリング終了

--- 3DGame/Util/game.h
+++ 3DGame/Util/game.h
@@ -9,10 +9,10 @@ namespace Game
 	constexpr bool kWindowModo = false;
 #endif
 	// ウインドウ名
-	const char* const kTitleText = "落ちるな！";
+	const char* const kTitleText = "Don't Fall";
 	// ウインドウサイズ
 	constexpr int kScreenWidth = 1280;
 	constexpr int kScreenHeight = 720;
 	// カラーモード
-	constexpr int kColorDepth = 32;		// 32 or 16
+	constexpr int kColorDepth = 32;
 };
