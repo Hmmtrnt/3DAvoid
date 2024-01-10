040d6da3b51561ff4eaf5c6476b40b37601dae3a
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-01-10 12:05:33 +0900

    コメント追加

--- 3DGame/Char/Enemy/EnemyBase.h
+++ 3DGame/Char/Enemy/EnemyBase.h
@@ -12,10 +12,10 @@ public:
 	EnemyBase();
 	virtual ~EnemyBase();
 
-	void Init(){}
-	void End(){}
-	void Update(){}
-	void Draw(){}
+	void Init(){}	// 初期化
+	void End(){}	// 終了時
+	void Update(){}	// 更新処理
+	void Draw(){}	// 描画
 
 	// エネミーの動き
 	void EnemyMove();
