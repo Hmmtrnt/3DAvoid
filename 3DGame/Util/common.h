9923212df45075a1c7f175253238ce07a1e56dd3
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-01-10 11:46:28 +0900

    commonリファクタリング完了

--- 3DGame/Util/common.h
+++ 3DGame/Util/common.h
@@ -1,3 +1,4 @@
+// 共通変数
 #pragma once
 #include "DxLib.h"
 #include "Pad.h"
@@ -6,11 +7,11 @@
 // 色
 namespace Color
 {
-	const int kWhite = 0xffffff;// 白
-	const int kBlack = 0x000000;// 黒
-	const int kRed = 0xff0000;// 赤
-	const int kCoral = 0xff7f50;// かなり薄いオレンジ
-	const int kTomato = 0xff6347;// トマトみたいな赤さ
+	const int kWhite = 0xffffff;	// 白
+	const int kBlack = 0x000000;	// 黒
+	const int kRed = 0xff0000;		// 赤
+	const int kTomato = 0xff6347;	// 若干薄い赤
+	const int kCoral = 0xff7f50;	// 赤よりのオレンジ
 	const int kOrangered = 0xff4500;// オレンジ
 }
 
